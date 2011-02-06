243914b4b9caa81e4bc15a5bcbb920771ab6bf71
sebsoler <sebastian.a.soler@gmail.com>
2011-02-06 18:10:48 -0500

    added a Posts resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  resources :posts
+
   resources :mircoposts
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110206225924) do
+ActiveRecord::Schema.define(:version => 20110206230725) do
 
   create_table "mircoposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at"
